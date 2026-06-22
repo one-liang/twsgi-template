07f7719b2f2b67a8e5972322081b7294d3ee8103
Liang <william810212@gmail.com>
2026-06-22 14:28:09 +0800

    feat(ui): 在多個頁面中新增浮動按鈕組件以增強用戶互動性

--- src/pages/news.vue
+++ src/pages/news.vue
@@ -1,9 +1,11 @@
 <script setup>
 import AppHeader from '../components/AppHeader.vue';
 import AppFooter from '../components/AppFooter.vue';
+import AppFloatButtons from '../components/AppFloatButtons.vue';
 </script>
 
 <template>
   <AppHeader />
+  <AppFloatButtons />
   <AppFooter />
 </template>
