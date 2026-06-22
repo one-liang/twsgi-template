996bd3cf159341c58495b74dee832723ccff44af
Liang <william810212@gmail.com>
2026-06-22 14:42:02 +0800

    feat(ui): 新增獎項卡片組件及相關樣式，整合至獲獎頁面以增強內容展示

---
+++ src/components/AppAwardCard.vue
@@ -0,0 +1,27 @@
+<script setup>
+defineProps({
+  image: { type: String, required: true },
+  title: { type: String, default: '' },
+  link: { type: String, default: '#' },
+});
+</script>
+
+<template>
+  <a class="award-card" :href="link">
+    <div class="award-card__media">
+      <img class="award-card__img" :src="image" :alt="title" />
+      <span class="award-card__mask"></span>
+    </div>
+    <div class="award-card__head">
+      <h4 class="award-card__title">{{ title }}</h4>
+      <span class="award-card__arrow">
+        <span class="btn-arrow">
+          <span class="btn-arrow__track">
+            <i class="fa-light fa-arrow-right-long"></i>
+            <i class="fa-light fa-arrow-right-long"></i>
+          </span>
+        </span>
+      </span>
+    </div>
+  </a>
+</template>
--- src/pages/awards.vue
+++ src/pages/awards.vue
@@ -1,9 +1,53 @@
 <script setup>
 import AppHeader from '../components/AppHeader.vue';
 import AppFooter from '../components/AppFooter.vue';
+import AppFloatButtons from '../components/AppFloatButtons.vue';
+import AppBanner from '../components/AppBanner.vue';
+import AppAwardCard from '../components/AppAwardCard.vue';
+import sectionBg from '@/assets/images/awards/award-section-bg.svg';
+import religiousWelfare from '@/assets/images/awards/award-religious-welfare.jpg';
+import nationalContribution from '@/assets/images/awards/award-national-contribution.jpg';
+import religiousDeepening from '@/assets/images/awards/award-religious-deepening.jpg';
+import socialEducation from '@/assets/images/awards/award-social-education.jpg';
+import wenxin from '@/assets/images/awards/award-wenxin.jpg';
+import artEducation from '@/assets/images/awards/award-art-education.jpg';
+
+const awardCards = [
+  { image: religiousWelfare, title: '宗教公益獎', link: '#' },
+  { image: nationalContribution, title: '全國性社會團體公益貢獻獎', link: '#' },
+  { image: religiousDeepening, title: '宗教公益深耕獎', link: '#' },
+  { image: socialEducation, title: '社會教育貢獻獎', link: '#' },
+  { image: wenxin, title: '文馨獎', link: '#' },
+  { image: artEducation, title: '藝術教育貢獻獎', link: '#' },
+];
 </script>
 
 <template>
   <AppHeader />
+  <AppBanner title="獲獎與肯定" :breadcrumb="[{ label: '獲獎與肯定' }]" />
+
+  <section class="awards-honor">
+    <!-- 裝飾圖 -->
+    <img class="awards-honor__bg" :src="sectionBg" alt="" />
+
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-12 col-lg-10">
+          <p class="awards-honor__lead h3">
+            創價會員秉持佛法慈悲精神積極關懷社會的行動，受到政府各部門肯定，屢屢獲獎。
+          </p>
+          <div class="awards-honor__panel">
+            <div class="row g-0">
+              <div v-for="card in awardCards" :key="card.title" class="col-12 col-lg-6">
+                <AppAwardCard :image="card.image" :title="card.title" :link="card.link" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <AppFloatButtons />
   <AppFooter />
 </template>
